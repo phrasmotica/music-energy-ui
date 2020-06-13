5d22f5d969389a4320691befa22b77ac1d2d6a41
Julian Lawrance <julian.lawrance@gmail.com>
2020-06-13 21:09:57 +0100

    Add basic track metadata to response model

--- src/TrackResponse.ts
+++ src/TrackResponse.ts
@@ -1,53 +1,113 @@
-/// <summary>
-/// Model for responses.
-/// </summary>
+/**
+ * Model for responses.
+ */
 export class TrackResponse
 {
-    // Gets or sets the ID of the track.
+    /**
+     * Gets or sets the ID of the track.
+     */
     id?: string
 
-    // Gets or sets the acousticness of the track.
+    /**
+     * Gets or sets the name of the track.
+     */
+    name?: string
+
+    /**
+     * Gets or sets the artist of the track.
+     */
+    artist?: string
+
+    /**
+     * Gets or sets the album of the track.
+     */
+    album?: string
+
+    /**
+     * Gets or sets the year of the track.
+     */
+    year?: number
+
+    /**
+     * Gets or sets the URL of artwork for the track.
+     */
+    artworkUrl?: string
+
+    /**
+     * Gets or sets the acousticness of the track.
+     */
     acousticness?: number
 
-    // Gets or sets the danceability of the track.
+    /**
+     * Gets or sets the danceability of the track.
+     */
     danceability?: number
 
-    // Gets or sets the energy of the track.
+    /**
+     * Gets or sets the energy of the track.
+     */
     energy?: number
 
-    // Gets or sets the instrumentalness of the track.
+    /**
+     * Gets or sets the instrumentalness of the track.
+     */
     instrumentalness?: number
 
-    // Gets or sets the liveness of the track.
+    /**
+     * Gets or sets the liveness of the track.
+     */
     liveness?: number
 
-    // Gets or sets the normalised loudness of the track.
+    /**
+     * Gets or sets the normalised loudness of the track.
+     */
     normalisedLoudness?: number
 
-    // Gets or sets the speechiness of the track.
+    /**
+     * Gets or sets the speechiness of the track.
+     */
     speechiness?: number
 
-    // Gets or sets the valence of the track.
+    /**
+     * Gets or sets the valence of the track.
+     */
     valence?: number
 
-    // Gets or sets the Monday energy score of the track.
+    /**
+     * Gets or sets the Monday energy score of the track.
+     */
     mondayEnergy?: number
 
-    // Gets or sets the Tuesday energy score of the track.
+    /**
+     * Gets or sets the Tuesday energy score of the track.
+     */
     tuesdayEnergy?: number
 
-    // Gets or sets the Wednesday energy score of the track.
+    /**
+     * Gets or sets the Wednesday energy score of the track.
+     */
     wednesdayEnergy?: number
 
-    // Gets or sets the Thursday energy score of the track.
+    /**
+     * Gets or sets the Thursday energy score of the track.
+     */
     thursdayEnergy?: number
 
-    // Gets or sets the Friday energy score of the track.
+    /**
+     * Gets or sets the Friday energy score of the track.
+     */
     fridayEnergy?: number
 
-
+    /**
+     * Constructor.
+     */
     constructor(
         id?: string,
+        name?: string,
+        artist?: string,
+        album?: string,
+        year?: number,
+        artworkUrl?: string,
         acousticness?: number,
         danceability?: number,
         energy?: number,
@@ -63,6 +123,11 @@ export class TrackResponse
         fridayEnergy?: number,
     ) {
         this.id = id
+        this.name = name
+        this.artist = artist
+        this.album = album
+        this.year = year
+        this.artworkUrl = artworkUrl
         this.acousticness = acousticness
         this.danceability = danceability
         this.energy = energy
@@ -78,10 +143,17 @@ export class TrackResponse
         this.fridayEnergy = fridayEnergy
     }
 
-
+    /**
+     * Returns a new instance from the given track response.
+     */
     static from(trackData: TrackResponse) {
         return new TrackResponse(
             trackData.id,
+            trackData.name,
+            trackData.artist,
+            trackData.album,
+            trackData.year,
+            trackData.artworkUrl,
             trackData.acousticness,
             trackData.danceability,
             trackData.energy,
